a5ea0772f6f12d21fc1badd5b1c064b16522ba5e
pchanndino-wq <pchanndino@gmail.com>
2026-01-21 11:15:13 -0800

    Set up React application entry point

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./src/App.js";
+
+const rootEl = document.getElementById("root");
+if (!rootEl) {
+  throw new Error("Root element #root not found");
+}
+
+createRoot(rootEl).render(React.createElement(App));
 
